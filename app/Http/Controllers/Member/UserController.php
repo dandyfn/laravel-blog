bf9fbfbedcba0c113104865cd36ed6a8be792014
Dandy-dashline <dandy.alfarisi18@gmail.com>
2026-05-10 22:05:53 +0700

    Form Edit Data User

--- app/Http/Controllers/Member/UserController.php
+++ app/Http/Controllers/Member/UserController.php
@@ -54,6 +54,8 @@ class UserController extends Controller
     public function edit(User $user)
     {
         //
+        $data = $user;
+        return view('member.users.edit',compact('data'));
     }
 
     /**
